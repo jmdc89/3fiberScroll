c89ac276504e546882ed557dcb2f0f763873d59a
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-08 14:38:54 +0300

    Update Overlay.jsx
    sections

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -36,8 +36,58 @@ export const Overlay = () => {
   });
 
     return (
-    <Scroll html><h1 className="font-serif text-2xl">Hello World
-    </h1>
-    </Scroll>
+        <Scroll html>
+        <div class="w-screen">
+          <Section opacity={opacityFirstSection}>
+            <h1 className="font-semibold font-serif text-2xl">
+              Hello, I'm Wawa Sensei
+            </h1>
+            <p className="text-gray-500">Welcome to my beautiful portfolio</p>
+            <p className="mt-3">I know:</p>
+            <ul className="leading-9">
+              <li>🧑‍💻 How to code</li>
+              <li>🧑‍🏫 How to learn</li>
+              <li>📦 How to deliver</li>
+            </ul>
+            <p className="animate-bounce  mt-6">↓</p>
+          </Section>
+          <Section right opacity={opacitySecondSection}>
+            <h1 className="font-semibold font-serif text-2xl">
+              Here are my skillsets 🔥
+            </h1>
+            <p className="text-gray-500">PS: I never test</p>
+            <p className="mt-3">
+              <b>Frontend 🚀</b>
+            </p>
+            <ul className="leading-9">
+              <li>ReactJS</li>
+              <li>React Native</li>
+              <li>VueJS</li>
+              <li>Tailwind</li>
+            </ul>
+            <p className="mt-3">
+              <b>Backend 🔬</b>
+            </p>
+            <ul className="leading-9">
+              <li>NodeJS</li>
+              <li>tRPC</li>
+              <li>NestJS</li>
+              <li>PostgreSQL</li>
+            </ul>
+            <p className="animate-bounce  mt-6">↓</p>
+          </Section>
+          <Section opacity={opacityLastSection}>
+            <h1 className="font-semibold font-serif text-2xl">
+              🤙 Call me maybe?
+            </h1>
+            <p className="text-gray-500">
+              I'm very expensive but you won't regret it
+            </p>
+            <p className="mt-6 p-3 bg-slate-200 rounded-lg">
+              📞 <a href="tel:(+42) 4242-4242-424242">(+42) 4242-4242-424242</a>
+            </p>
+          </Section>
+        </div>
+      </Scroll>
     );
 }
